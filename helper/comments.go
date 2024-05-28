5febeeb6072f34e8db9b32fb3a538c1cea70fc6e
EmbraceTheLight <1010642166@qq.com>
2024-05-28 10:18:29 +0800

    Add a function of get comments

--- helper/comments.go
+++ helper/comments.go
@@ -21,7 +21,7 @@ func GetCommentReplies(videoID, to int64) (ret []*EntitySets.CommentSummary, err
 // GetRootCommentsSummariesByVideoID 获得Root评论，即该评论不是回复其他评论的评论
 func GetRootCommentsSummariesByVideoID(videoID int64, order string, Page, CommentsNumbers int64) (ret []*EntitySets.CommentSummary, err error) {
 	if order == "default" || order == "likes" {
-		err = DAO.DB.Debug().Model(&EntitySets.Comments{}).Where("`video_id` = ? AND `To` = ?", videoID, -1).
+		err = DAO.DB.Debug().Model(&EntitySets.Comments{}).Where("`video_id` = ? AND `to` = ?", videoID, -1).
 			Order("likes DESC").Offset(int(Page)).Limit(int(CommentsNumbers)).Find(&ret).Error
 	} else if order == "newest" {
 		err = DAO.DB.Model(&EntitySets.Comments{}).Where("`video_id` = ? AND `To` = ?", videoID, -1).
